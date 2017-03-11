bd5e8c9e4faf363031ebb8113513f006fee9ae2c
breno morais <breno.lopes.morais@gmail>
2017-03-11 02:08:52 -0300

    tipagem dinamica

---
+++ tipagem-dinamica/tipagem-dinamica.js
@@ -0,0 +1,11 @@
+var idade = 22;
+console.log(typeof idade);
+console.log('Idade: ',idade);
+
+idade += ' Anos.';
+console.log('Idade: ',idade);
+console.log(typeof idade);
+
+idade = false
+console.log('Idade: ',idade);
+console.log(typeof idade);
