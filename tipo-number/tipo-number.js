1f6e346987a8361b79d2a98f579461ed54ecdd87
breno morais <breno.lopes.morais@gmail>
2017-03-11 02:17:27 -0300

    tipo number

---
+++ tipo-number/tipo-number.js
@@ -0,0 +1,7 @@
+// E javaScript  todos numero é do tipo number não tem tipo de dados como double,decimal e outros.
+
+var idade = 22;
+var x = 0.3;
+var soma = (0.3 * 10 - 0.5);
+
+console.log(typeof idade, x, soma);
