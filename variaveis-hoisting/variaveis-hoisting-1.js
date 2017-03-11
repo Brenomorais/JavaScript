a4852f5c264ff1abc11d3adff20fe7f43c3b6dea
breno morais <breno.lopes.morais@gmail>
2017-03-11 02:00:10 -0300

    variaveis e hoisting

---
+++ variaveis-hoisting/variaveis-hoisting-1.js
@@ -0,0 +1,16 @@
+var percentualImposto = 0.15;
+var valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar:', totalImposto);
+
+// Hoisting = leva todoas a desclarações de variaveis para o escopo do script mas nao considera a atribuição dos valores
+
+
+/*
+var percentualImposto, valor, totalImposto;
+percentualImposto = 0.15;
+valor = 100;
+totalImposto = valor * percentualImposto;
+console.log('Total imposto a pagar', totalImposto);
+*/
