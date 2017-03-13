b6be471514c7e5f2b4ace00d7b39ea9d382e0ee3
breno morais <breno.lopes.morais@gmail>
2017-03-13 14:33:43 -0300

    função matematicas oferecidas pelo javascript

---
+++ funcoes-matematicas/funcoes-matematicas.js
@@ -0,0 +1,20 @@
+//Função menor numero
+var menorNota = Math.min(9,2,1,8,6,5,0.5);
+console.log('Menor Nota', menorNota);
+
+//Função maior numero
+var maiorNota = Math.max(9,2,1,8,6,5,0.5);
+console.log('Maior Nota', maiorNota);
+
+//Função de arredondamento
+var temperatura = Math.round(22.6);
+console.log('Temperatura', temperatura);
+
+//Função valor aleatorio
+var valorAleatorio = Math.random()
+console.log('valor aletório',valorAleatorio);
+var  numero = valorAleatorio * 60;
+console.log('Número', numero);
+var numeroMega = Math.floor(numero)+1;
+console.log('Número da mega', numeroMega);
+
