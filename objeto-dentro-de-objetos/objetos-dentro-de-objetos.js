c4cb915081d0f13fba074e562acbcc2ad8a8adaa
breno morais <breno.lopes.morais@gmail>
2017-03-14 01:53:52 -0300

    criando objeto dentro de objeto

---
+++ objeto-dentro-de-objetos/objetos-dentro-de-objetos.js
@@ -0,0 +1,15 @@
+var joao = {
+	nome: 'João',
+	idade:'25',
+	endereco: {
+		logradouro: 'Av Brasil',
+		numero: '100',
+		complemento: 'Apto 30',
+		cidade: 'São Paulo',
+		estado: 'SP'
+	}
+}
+
+console.log(joao);
+
+console.log('Cidade do João: ', joao.endereco.cidade);
