90c5716094024a9feddba9293767febabffd93f1
breno morais <breno.lopes.morais@gmail>
2017-03-14 15:12:12 -0300

    criando arrays

---
+++ criando-arrays/criando-arrary.js
@@ -0,0 +1,14 @@
+var emails = [];
+emails[0] = 'breno@email.com';
+emails[1] = 'magda@email.com';
+emails[2] = 'mae@email.com';
+
+console.log('emails[0]',emails[0]);
+console.log('emails[5]',emails[5]);
+
+var nomes = ['Breno','Magda'];
+console.log('Total Nomes', nomes.length);
+
+nomes[4] = 'José';
+console.log('Nomes depois do Jose', nomes.length);
+console.log('nome[3]', nomes[3]);
