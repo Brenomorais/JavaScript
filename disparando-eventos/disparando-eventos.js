cda6264ed8ef8c1c991e08f1db50cd464845fbe0
breno morais <breno.lopes.morais@gmail>
2017-03-15 18:17:52 -0300

    eventos de manipulacao

--- disparando-eventos/disparando-eventos.js
+++ disparando-eventos/disparando-eventos.js
@@ -1,4 +1,28 @@
 $(function() {
+	var atividade = $('#atividade');
+	var listaAtividades = $('#lista-atividades');
+
+	atividade.on('keyup', function(e) {
+		var keyCode = e.keyCode;
+
+		if (keyCode === 13) {
+			atividade.trigger('enter', atividade.val());
+			atividade.val('');
+		}
+	});
+
+	atividade.on('enter', function(evento, texto) {
+		listaAtividades.prepend('<li>' + texto + '</li>');
+	});
+
+	atividade.on('enter', function(eventp, texto) {
+		console.log('Outra funcionalidade aqui...');
+	});
+
+});
+
+/*
+$(function() {
 	
 	var atividade = $('#atividade');
 	var listaAtividades = $('#lista-atividades');
@@ -18,3 +42,4 @@ $(function() {
 	}
 
 });
+*/
